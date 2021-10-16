a558c55410996b7fd292a64a8471d5eab624fde6
ccsunnyfd <chzhyfd@126.com>
2021-10-16 18:52:03 +0800

    tracing[main.go] add name and version for tracing

--- app/user/service/cmd/server/main.go
+++ app/user/service/cmd/server/main.go
@@ -21,9 +21,9 @@ import (
 // go build -ldflags "-X main.Version=x.y.z"
 var (
 	// Name is the name of the compiled software.
-	Name string
+	Name = "user"
 	// Version is the version of the compiled software.
-	Version string
+	Version = "v1.0.0"
 	// flagconf is the config flag.
 	flagconf string
 
